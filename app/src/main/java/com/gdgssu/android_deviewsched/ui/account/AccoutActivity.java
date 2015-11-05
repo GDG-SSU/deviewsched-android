fec8446d1df64d6063a5cdb627e8c5d67b6762b9
Ace Kim <ace.jckim@gmail.com>
2015-11-05 15:53:02 +0900

    AccoutActivity 생성

---
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/account/AccoutActivity.java
@@ -0,0 +1,15 @@
+package com.gdgssu.android_deviewsched.ui.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gdgssu.android_deviewsched.R;
+
+public class AccoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accout);
+    }
+}
