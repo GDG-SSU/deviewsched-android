ac2d787b0bbe963ce676a97f767627ea69976245
Ace Kim <ace.jckim@gmail.com>
2015-11-05 20:25:25 +0900

    Develop Account : 페이스북 로그인 로직 개발

--- app/src/main/java/com/gdgssu/android_deviewsched/DeviewSchedApplication.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/DeviewSchedApplication.java
@@ -1,18 +1,22 @@
 package com.gdgssu.android_deviewsched;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.android.volley.RequestQueue;
+import com.facebook.FacebookSdk;
 import com.gdgssu.android_deviewsched.helper.FavoritePreferenceHelper;
 import com.gdgssu.android_deviewsched.helper.LoginPreferenceHelper;
 
 import com.gdgssu.android_deviewsched.model.sessioninfo.AllScheItems;
 import com.gdgssu.android_deviewsched.model.sessioninfo.Day;
+import com.navercorp.volleyextensions.volleyer.factory.DefaultRequestQueueFactory;
 
 import java.util.ArrayList;
 
-public class DeviewSchedApplication extends Application{
+public class DeviewSchedApplication extends Application {
 
+    private Context GLOBAL_CONTEXT = null;
     public static Boolean LOGIN_STATE = false;
     public static boolean FAVOR_SESSION_STATE = false;
     public static final String HOST_URL = "http://deview.unstabler.pl/";
@@ -26,21 +30,30 @@ public class DeviewSchedApplication extends Application{
     public void onCreate() {
         super.onCreate();
 
-//        FacebookSdk.sdkInitialize(getApplicationContext());
-//        setLoginState();
-//        setFavorSessionState();
+        GLOBAL_CONTEXT = getApplicationContext();
+
+        FacebookSdk.sdkInitialize(GLOBAL_CONTEXT);
+        setLoginState();
+        setFavorSessionState();
+        initRequestQueue();
+
     }
 
     public void setLoginState() {
         LoginPreferenceHelper prefHelper = new LoginPreferenceHelper(getBaseContext());
-        LOGIN_STATE = prefHelper.getPrefLoginValue(LoginPreferenceHelper.PREF_LOGIN_STATE, false);
+        DeviewSchedApplication.LOGIN_STATE = prefHelper.getPrefLoginValue(LoginPreferenceHelper.PREF_LOGIN_STATE, false);
     }
 
-    public void setFavorSessionState(){
+    public void setFavorSessionState() {
         /**
-         * 로그인할떄 false로 돌려야함
+         * 로그인할때 false로 돌려야함
          */
         FavoritePreferenceHelper prefHelper = new FavoritePreferenceHelper(getBaseContext());
-        FAVOR_SESSION_STATE = prefHelper.getFavorSessionState(FavoritePreferenceHelper.PREF_FAVOR_STATE);
+        DeviewSchedApplication.FAVOR_SESSION_STATE = prefHelper.getFavorSessionState(FavoritePreferenceHelper.PREF_FAVOR_STATE);
+    }
+
+    public void initRequestQueue() {
+        deviewRequestQueue = DefaultRequestQueueFactory.create(GLOBAL_CONTEXT);
+        deviewRequestQueue.start();
     }
 }
--- app/src/main/java/com/gdgssu/android_deviewsched/ui/account/AccoutActivity.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/account/AccoutActivity.java
@@ -1,19 +1,85 @@
 package com.gdgssu.android_deviewsched.ui.account;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.gdgssu.android_deviewsched.DeviewSchedApplication;
 import com.gdgssu.android_deviewsched.R;
+import com.gdgssu.android_deviewsched.helper.FavoritePreferenceHelper;
+import com.gdgssu.android_deviewsched.helper.LoginPreferenceHelper;
 
+import static com.gdgssu.android_deviewsched.util.LogUtils.LOGE;
+import static com.gdgssu.android_deviewsched.util.LogUtils.LOGI;
 import static com.gdgssu.android_deviewsched.util.LogUtils.makeLogTag;
 
-public class AccoutActivity extends AppCompatActivity {
+public class AccoutActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
     private static final String TAG = makeLogTag("AccoutActivity");
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accout);
+
+        initView();
+
+        callbackManager = CallbackManager.Factory.create();
+        setLoginState();
+        setFavorSessionState();
+    }
+
+    private void initView() {
+        final LoginButton fbLoginButton = (LoginButton)findViewById(R.id.account_facebooklogin);
+        fbLoginButton.setReadPermissions("user_friends");
+        fbLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbLoginButton.registerCallback(callbackManager, AccoutActivity.this);
+            }
+        });
+    }
+
+    public void setLoginState() {
+        LoginPreferenceHelper prefHelper = new LoginPreferenceHelper(getBaseContext());
+        DeviewSchedApplication.LOGIN_STATE = prefHelper.getPrefLoginValue(LoginPreferenceHelper.PREF_LOGIN_STATE, false);
+    }
+
+    public void setFavorSessionState() {
+        /**
+         * 로그인할떄 false로 돌려야함
+         */
+        FavoritePreferenceHelper prefHelper = new FavoritePreferenceHelper(getBaseContext());
+        DeviewSchedApplication.FAVOR_SESSION_STATE = prefHelper.getFavorSessionState(FavoritePreferenceHelper.PREF_FAVOR_STATE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+        LOGI(TAG, "onSuccess");
+    }
+
+    @Override
+    public void onCancel() {
+        LOGI(TAG, "onCancel");
+    }
+
+    @Override
+    public void onError(FacebookException error) {
+        LOGE(TAG, "onError", error.getCause());
     }
 }
