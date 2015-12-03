b42771918d638e7e319ddce7e67a3bbbd53007ff
Ace Kim <ace.jckim@gmail.com>
2015-12-03 16:28:38 +0900

    View : Complete login and setting user profile
    Dialog를 이용한 로그인과, 유저의 사진과 이름을 설정하는 기능 개발 완료

--- app/src/main/java/com/gdgssu/android_deviewsched/helper/UserProfileProvider.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/helper/UserProfileProvider.java
@@ -1,18 +1,22 @@
 package com.gdgssu.android_deviewsched.helper;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.facebook.Profile;
 import com.gdgssu.android_deviewsched.model.User;
 import com.gdgssu.android_deviewsched.util.DptopxChanger;
 
 public class UserProfileProvider {
+
     public static User getUserProfile(Context context, int dp) {
         int size = DptopxChanger.dpToPXChange(context, dp);
         User user = new User();
         Profile profile = Profile.getCurrentProfile();
-        user.name = profile.getName();
-        user.picture = profile.getProfilePictureUri(size, size).toString();
+        if (profile!=null){
+            user.name = profile.getName();
+            user.picture = profile.getProfilePictureUri(size, size).toString();
+        }
 
         return user;
     }
