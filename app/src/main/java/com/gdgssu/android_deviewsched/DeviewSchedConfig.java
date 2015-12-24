4be975bc1e0df002dacb74821aebf870c44dbcca
Ace <ace.jckim@gmail.com>
2015-12-24 13:03:00 +0900

    Feature : Develop LocationActivity with Google Maps API
    Google Maps API를 이용하여 행사장을 안내하는 LocationActivity를 개발하였습니다.

--- app/src/main/java/com/gdgssu/android_deviewsched/DeviewSchedConfig.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/DeviewSchedConfig.java
@@ -2,4 +2,5 @@ package com.gdgssu.android_deviewsched;
 
 public class DeviewSchedConfig {
     public static final String FACEBOOK_APP_SECRET = "fb7e42f07fcc9a093ff2a93a3bd36e82";
+    public static final String NAVER_MAP_API_KEY = "98ebf1b17fa00b143b8d88b250bf9390";
 }
--- app/src/main/java/com/gdgssu/android_deviewsched/ui/location/LocationActivity.java
+++ app/src/main/java/com/gdgssu/android_deviewsched/ui/location/LocationActivity.java
@@ -1,13 +1,19 @@
 package com.gdgssu.android_deviewsched.ui.location;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.gdgssu.android_deviewsched.R;
-import com.nhn.android.maps.NMapActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import static com.gdgssu.android_deviewsched.util.LogUtils.makeLogTag;
 
-public class LocationActivity extends NMapActivity {
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = makeLogTag("LocationActivity");
 
@@ -16,11 +22,15 @@ public class LocationActivity extends NMapActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    protected void onStop() {
-
-        super.onStop();
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng deview = new LatLng(127.0594275, 37.5116621);
+        googleMap.addMarker(new MarkerOptions().position(deview).title("Deview 2016"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(deview));
     }
 }
